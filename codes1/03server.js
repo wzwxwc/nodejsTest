3469a05186b8a50e31d325157c1d3ab46847c41c
wzwxwc <zc_1987>
2017-06-03 07:18:31 +0800

    mongo和mysql的数据读写

--- codes1/03server.js
+++ codes1/03server.js
@@ -7,12 +7,10 @@ var http = require('http');
 function start() {
     http.createServer(function (request, response) {
         console.log("request received");
-
         // 发送 HTTP 头部
         // HTTP 状态值: 200 : OK
         // 内容类型: text/plain
         response.writeHead(200, {'Content-Type': 'text/plain'});
-
         // 发送响应数据 "Hello World"
         response.end('Hello World zc\n');
         // response.end();
@@ -21,4 +19,5 @@ function start() {
     // 终端打印如下信息
     console.log('Server running at http://127.0.0.1:8888/');
 }
+start();
 exports.start = start;
--- grabData/mysqlRead.js
+++ grabData/mysqlRead.js
@@ -1,31 +1,40 @@
 /**
  * Created by zc on 2017/6/2.
+ * 问题：
+ * 1、这个程序进行web请求之后，为什么在标题栏一直是loading的图标？？
+ * 2、返回结果是乱码的问题
  */
-var mysql=require("mysql");
-var Client = mysql.Client;
-var client = new Client();
-client.user = 'root';
-client.password = 'root';
-console.log('Connecting to MySQL...');
-//使用数据库zctest
-client.query('USE zctest');
-http = require("http");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'zctest'
+});
+
+var http = require("http");
 var server = http.createServer(function (request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    client.query('SELECT * FROM people', function selectCb(err, results, fields) {
-        if (err) {
-            throw err;
-        }
+    connection.connect();
+    console.log("mysql 数据库连接成功");
+
+    response.writeHead(200, {"Content-Type": "text/html", "charset": "utf-8"});
+    console.log("开始处理web请求")
+    connection.query('SELECT * FROM people', function (error, results, fields) {
+        console.log("完成数据库查询");
+        if (error) throw error;
         var data = '';
         for (var i = 0; i < results.length; i++) {
             var firstResult = results[i];
-            data += 'id: ' + firstResult['id'] + 'tag: ' + firstResult['tag'];
+            data += 'id: ' + firstResult['id'] + 'name: ' + firstResult['name'];
         }
         response.write(data);
+        //如果没有下述这个语句，则浏览器的标题会一直旋转
         response.end();
+
+        connection.end();
+        console.log("关闭数据库连接");
     });
 });
-
-server.listen(8080);
-var sys = require("util");
-sys.puts("Server running at http://localhost:8080/");
+server.listen(9966);
+// var sys = require("util");
+// sys.puts("Server running at http://localhost:8080/");

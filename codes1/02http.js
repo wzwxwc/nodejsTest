d557968c9b712b4f40ee403f6f0e7a3213fc7881
wzwxwc <zc_1987>
2017-06-05 09:04:29 +0800

    重命名调整顺序

--- codes1/02http.js
+++ codes1/02http.js
@@ -1,8 +1,8 @@
 var http = require("http");
-http.createServer(fnDealHttp).listen("9999");
+http.createServer(fnDealHttp).listen("6666");
 function fnDealHttp(request, response) {
     response.write("hello zc,good morning,zc is the best man");
     console.log("fnDealHttp is executed 222222222222");
     response.end();
 }
-console.log("server is running at port 9999");
+console.log("server is running at port 6666");
--- codes1/event1.js
+++ codes1/event1.js
@@ -1,7 +1,9 @@
 /**
  * Created by zc on 2017/5/22.
  */
-var EventEmitter = require("events").EventEmitter;
+var EventEmitter = require("events");
+//下述写法是老版本的的写法，新版本也同样兼容下述写法
+// var EventEmitter = require("events").EventEmitter;
 var emitter = new EventEmitter();
 emitter.on("haha", function () {
     console.log("haha is executed");

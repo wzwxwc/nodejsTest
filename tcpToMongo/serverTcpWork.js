32ee10bc2d590ba27051fc96fc0efa32d45499b4
wzwxwc <zc_1987>
2017-08-31 19:16:30 +0800

    no comments from zcRescuer

--- tcpToMongo/serverTcpWork.js
+++ tcpToMongo/serverTcpWork.js
@@ -11,6 +11,7 @@ var HOST = '192.168.20.193';
 var PORT = 8099;
 //没启动服务，竟然也可以连接成功
 var DB_URL = "mongodb://localhost:27017/zcuse";
+var m_collName = "nd";
 var dbMongo = null;
 var oneTravelId = null;
 
@@ -28,6 +29,7 @@ function fnCreateTcpServer() {
         }
         console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
         sock.on('data', function (data) {
+            // console.log(data.toString());
             fnDealWithTcpData(data.toString());
         });
         sock.on('close', function (data) {
@@ -47,47 +49,52 @@ function fnDealWithTcpData(strDataTcp) {
     for (var i = 0; i < arrData.length; i++) {
         var strOneData = arrData[i];
         if (strOneData.indexOf("$GPRMC") > 0) {
-            var oneDoc = {};
-            //空气中甲烷的浓度（单位ppm）
-            oneDoc.tag.CONCENTRATION = strOneData.substring(0, strOneData.indexOf(";"));
-            var arrItem = strOneData.split(",");
-            //时分秒（改成了北京时间）
-            oneDoc.tag.UTCTIME = arrItem[1];
-            //有效性
-            oneDoc.tag.VALIDITYSTATUS = arrItem[2];
-            //纬度（需要转换）
-            var LATITUDE = fnDealWithLatLng(arrItem[3]);
-            oneDoc.tag.LATITUDE_ORIGINAL = arrItem[3];
-            //纬度方向
-            oneDoc.tag.NORTHORSOUTH = arrItem[4];
-            //经度（需要转换）
-            var LONGITUDE = fnDealWithLatLng(arrItem[5]);
-            oneDoc.tag.LONGITUDE_ORIGINAL = arrItem[5];
-            //存储地理字段
-            oneDoc.geom = fnCreateGeojson(LATITUDE, LONGITUDE);
-            //经度方向
-            oneDoc.tag.EASTORWEST = arrItem[6];
-            //地面速度
-            oneDoc.tag.GROUNDSPEED = arrItem[7];
-            //速度方向
-            oneDoc.tag.SPEEDDIRECTION = arrItem[8];
-            //月日年
-            oneDoc.tag.DATE = arrItem[9];
-            //处理后的日期时间格式
-            oneDoc.tag.DATETIME = fnDealWithTime(oneDoc.tag.UTCTIME, oneDoc.tag.DATE);
-            //磁偏角
-            oneDoc.tag.DECLINATION = arrItem[10];
-            //磁偏角方向
-            oneDoc.tag.DECLINATIONDIRECTION = arrItem[11];
-            //模式指示及校验和
-            oneDoc.tag.MODEINDICATION = arrItem[12];
-            //监测类型（如 甲烷CH4， 乙炔）
-            oneDoc.tag.TYPE = "CH4";
-            //数据来源公司
-            oneDoc.tag.FIRMNAME = "大方数据";
-            //一次轨迹的编号（序列编号）
-            oneDoc.tag.LISTCODE = oneTravelId;
-            insertData(oneDoc);
+            try {
+                var oneDoc = {};
+                oneDoc.tag = {};
+                //空气中甲烷的浓度（单位ppm）
+                oneDoc.tag.CONCENTRATION = strOneData.substring(0, strOneData.indexOf(";"));
+                var arrItem = strOneData.split(",");
+                //时分秒（改成了北京时间）
+                oneDoc.tag.UTCTIME = arrItem[1];
+                //有效性
+                oneDoc.tag.VALIDITYSTATUS = arrItem[2];
+                //纬度（需要转换）
+                var LATITUDE = fnDealWithLatLng(arrItem[3]);
+                oneDoc.tag.LATITUDE_ORIGINAL = arrItem[3];
+                //纬度方向
+                oneDoc.tag.NORTHORSOUTH = arrItem[4];
+                //经度（需要转换）
+                var LONGITUDE = fnDealWithLatLng(arrItem[5]);
+                oneDoc.tag.LONGITUDE_ORIGINAL = arrItem[5];
+                //存储地理字段
+                oneDoc.geom = fnCreateGeojson(LATITUDE, LONGITUDE);
+                //经度方向
+                oneDoc.tag.EASTORWEST = arrItem[6];
+                //地面速度
+                oneDoc.tag.GROUNDSPEED = arrItem[7];
+                //速度方向
+                oneDoc.tag.SPEEDDIRECTION = arrItem[8];
+                //月日年
+                oneDoc.tag.DATE = arrItem[9];
+                //处理后的日期时间格式
+                oneDoc.tag.DATETIME = fnDealWithTime(oneDoc.tag.UTCTIME, oneDoc.tag.DATE);
+                //磁偏角
+                oneDoc.tag.DECLINATION = arrItem[10];
+                //磁偏角方向
+                oneDoc.tag.DECLINATIONDIRECTION = arrItem[11];
+                //模式指示及校验和
+                oneDoc.tag.MODEINDICATION = arrItem[12];
+                //监测类型（如 甲烷CH4， 乙炔）
+                oneDoc.tag.TYPE = "CH4";
+                //数据来源公司
+                oneDoc.tag.FIRMNAME = "大方数据";
+                //一次轨迹的编号（序列编号）
+                oneDoc.tag.LISTCODE = oneTravelId;
+                insertData(oneDoc);
+            } catch (e) {
+                console.log("发生错误：" + e);
+            }
         } else {
             //对于浓度值为null和空的信息，不入库
         }
@@ -95,27 +102,40 @@ function fnDealWithTcpData(strDataTcp) {
 }
 
 function fnDealWithLatLng(latOrLang) {
-    var beforPoint = latOrLang.substring(0, latOrLang.indexOf("."));
-    while (beforPoint.length < 5) {
-        beforPoint = "0" + beforPoint;
+    var result = null;
+    try {
+        var beforPoint = latOrLang.substring(0, latOrLang.indexOf("."));
+        while (beforPoint.length < 5) {
+            beforPoint = "0" + beforPoint;
+        }
+        var afterPoint = latOrLang.substr(latOrLang.indexOf(".") + 1, 4);
+        var abc = parseFloat(beforPoint.substr(0, 3));
+        var de = parseFloat(beforPoint.substr(3, 2));
+        var fghi = parseFloat(afterPoint);
+        var result = abc + (de / 60) + (fghi / 600000);
+    } catch (e) {
+        console.log("经纬度转换发生错误：line117：" + e);
     }
-    var afterPoint = latOrLang.substr(latOrLang.indexOf(".") + 1, 4);
-    var abc = parseFloat(beforPoint.substr(0, 3));
-    var de = parseFloat(beforPoint.substr(3, 2));
-    var fghi = parseFloat(afterPoint);
-    var result = abc + (de / 60) + (fghi / 600000);
     return result;
 }
 
 function insertData(oneDoc) {
-    var colVip = dbMongo.collection('ndDingWei');
-    colVip.insert(oneDoc, function (error, result) {
-        if (error) {
-            console.log('Error:' + error);
-        } else {
-            console.log(result.result.n);
-        }
-    });
+    try {
+        var colVip = dbMongo.collection(m_collName);
+        colVip.insert(oneDoc, function (error, result) {
+            try {
+                if (error) {
+                    console.log('mongo写入数据发生错误:' + error);
+                } else {
+                    console.log(result.result.n);
+                }
+            } catch (e) {
+                console.log("发生错误：line133");
+            }
+        });
+    } catch (e) {
+        console.log("mongo存储发生错误：line128：" + e);
+    }
 }
 
 function fnCreateGeojson(lat, lng) {
@@ -128,13 +148,18 @@ function fnCreateGeojson(lat, lng) {
 function fnDealWithTime(utc_hms, mdy) {
     //除了下述这种方式，有没有更好的方法来生成想要的时间格式？
     //是否可以不去处理原始的内容，而只是以yymmdd的形式来表示？
-    var year = parseInt(mdy.substr(4, 2));
-    var month = parseInt(mdy.substr(0, 2));
-    var date = parseInt(mdy.substr(2, 2));
-    var h = parseInt(utc_hms.substr(0, 2)) + 8;
-    var m = parseInt(utc_hms.substr(2, 2));
-    var s = parseInt(utc_hms.substr(4, 2));
-    var ms = parseInt(utc_hms.substring(utc_hms.indexOf(".") + 1));
-    var result = new Date(year, month, date, h, m.s, ms);
+    var result = null;
+    try {
+        var year = parseInt(mdy.substr(4, 2)) + 2000;
+        var month = parseInt(mdy.substr(2, 2)) - 1;
+        var date = parseInt(mdy.substr(0, 2));
+        var h = parseInt(utc_hms.substr(0, 2)) + 8;
+        var m = parseInt(utc_hms.substr(2, 2));
+        var s = parseInt(utc_hms.substr(4, 2));
+        var ms = parseInt(utc_hms.substring(utc_hms.indexOf(".") + 1));
+        result = new Date(year, month, date, h, m, s, ms);
+    } catch (e) {
+        console.log("时间转换发生错误：line147：" + e);
+    }
     return result;
 }
--- webServiceToMongo/webserviceToMongo.js
+++ webServiceToMongo/webserviceToMongo.js
@@ -2,10 +2,22 @@
  * Created by zc on 2017/8/29.
  */
 var soap = require('soap');
-var url = 'http://120.27.19.171:8686/Service/GasService?wsdl';
-var args = {userid: 'bjdata'};
-soap.createClient(url, function (err, client) {
-    client.getJksbList(args, function (err, result) {
-        console.log(result);
-    });
+var url = 'http://120.27.19.171:8686/Service/GasService/wsdl?wsdl';
+// var url = 'http://120.27.19.171:8686/Service/GasService?wsdl';
+// var args = {arg0: "bjdata"};
+var opts = {
+    "disableCache": true
+}
+soap.createClient(url, opts, function (err, client) {
+    if (err) {
+        console.log("发生错误:" + err);
+    } else {
+        client.getJksbList("bjdata", function (err2, result) {
+            if (err2) {
+                console.log("发生错误：" + err2);
+            } else {
+                console.log(result);
+            }
+        });
+    }
 });
